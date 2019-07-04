583ac4b208c2848172f8cc4eb4d9dde34fa783ef
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 00:16:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,11 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  return array.index(value_to_find)
 end
 
 def find_max_value(array)
   # Add your solution here
+  
 end
 
 def find_min_value(array)
